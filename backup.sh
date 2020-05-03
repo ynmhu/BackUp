aaba0a6d4429f7e659bcfd4ca9dd2d7a55c3b607
Markus Lajos <csibeszml@gmail.com>
2020-05-03 19:07:22 +0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,8 +3,8 @@
 #Author = Markus Lajos
 #Version 1.0 (Debian 10)
 #START
-TIME=`date +%I-%M-%b-%d-%y`            	  # Date to add filename
-SRCDIR=$HOME	          			            # Location of Important Data 
+TIME=`date +%I-%M-%b-%d-%y`               # Date to add filename
+SRCDIR=$HOME                              # Location of Important Data 
 IMDIR=cert								  # Important Dir Name
 FILENAME=backup-$TIME-$IMDIR.tar    	  # Backup Format
 DESDIR=/home/ynm/backup          		  # Destination of backup file (safe place)
